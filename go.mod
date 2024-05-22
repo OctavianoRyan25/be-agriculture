dd19e900c8b729bc8d1d1b61340b7a6553eb8776
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-05-22 14:56:57 +0700

    add entity and user request

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/cloudinary/cloudinary-go/v2 v2.7.0 // indirect
 	github.com/creasty/defaults v1.5.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
---
+++ modules/user/request.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"time"
+)
+
+type UserRequest struct {
+	Name       string `json:"name" valid:"required~Name is required"`
+	Email      string `json:"email" valid:"required~Email is required,email~Email is not valid"`
+	Password   string `json:"password" valid:"required~Password is required"`
+	Is_Active  bool   `json:"is_active"`
+	Url_Image  string `json:"url_image"`
+	Created_at time.Time
+	Updated_at time.Time
+}
