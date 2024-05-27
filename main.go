2d46cac60ceb852467c1c570e223907dbf2793bd
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-05-27 14:10:45 +0700

    add auth admin and user

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"net/http"
 
 	"github.com/OctavianoRyan25/be-agriculture/configs"
-	"github.com/labstack/echo"
+	"github.com/OctavianoRyan25/be-agriculture/middlewares"
+	"github.com/OctavianoRyan25/be-agriculture/modules/admin"
+	"github.com/OctavianoRyan25/be-agriculture/modules/user"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
@@ -24,7 +27,27 @@ func main() {
 		panic("Failed to migrate database")
 	}
 
-	e.GET("/", func(c echo.Context) error {
+	repo := user.NewRepository(db)
+	useCase := user.NewUseCase(repo)
+	controller := user.NewUserController(useCase)
+
+	repoAdmin := admin.NewRepository(db)
+	useCaseAdmin := admin.NewUseCase(repoAdmin)
+	controllerAdmin := admin.NewUserController(*useCaseAdmin)
+
+	group := e.Group("/api/v1")
+	group.POST("/register", controller.RegisterUser)
+	group.POST("/check-email", controller.CheckEmail)
+	group.POST("/verify", controller.VerifyEmail)
+	group.POST("/login", controller.Login)
+	group.GET("/profile", controller.GetUserProfile, middlewares.Authentication())
+
+	groupAdmin := e.Group("/api/v1/admin")
+	groupAdmin.POST("/register", controllerAdmin.RegisterUser)
+	groupAdmin.POST("/login", controllerAdmin.Login)
+	groupAdmin.GET("/profile", controllerAdmin.GetUserProfile, middlewares.Authentication())
+
+	group.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
---
+++ modules/user/mailer.go
@@ -0,0 +1,16 @@
+package user
+
+var (
+	//Testing
+	EMAIL_FROM    = "admin@admin.com"
+	SMTP_HOST     = "sandbox.smtp.mailtrap.io"
+	SMTP_PORT     = "2525"
+	SMTP_USER     = "b3f8c771a3ac6d"
+	SMTP_PASSWORD = "c0faf64735c303"
+
+	// EMAIL_FROM    = "mailtrap@tastedespair.site"
+	// SMTP_HOST     = "live.smtp.mailtrap.io"
+	// SMTP_PORT     = "2525"
+	// SMTP_USER     = "api"
+	// SMTP_PASSWORD = "31903f173fcfc189cf8164bacd19f407"
+)
