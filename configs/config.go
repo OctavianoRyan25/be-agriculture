6b8ae60bed3533eb2e496acf41106f055215c4a1
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-06-05 18:57:33 +0700

    bug-fix

--- configs/config.go
+++ configs/config.go
@@ -2,6 +2,7 @@ package configs
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,17 +10,18 @@ import (
 
 func InitDB() (*gorm.DB, error) {
 	//Konfigurasi koneksi ke database MySQL
-	// dbUser := os.Getenv("DB_USER")
-	// dbPass := os.Getenv("DB_PASS")
-	// dbHost := os.Getenv("DB_HOST")
-	// dbPort := os.Getenv("DB_PORT")
-	// dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-	dbUser := "root"
-	dbPass := ""
-	dbHost := "localhost"
-	dbPort := "3306"
-	dbName := "agriculture"
+	//Development
+	// dbUser := "root"
+	// dbPass := ""
+	// dbHost := "localhost"
+	// dbPort := "3306"
+	// dbName := "agriculture"
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 	//dsn := "root:@tcp(localhost:3306)/minpro?charset=utf8mb4&parseTime=True&loc=Local"
