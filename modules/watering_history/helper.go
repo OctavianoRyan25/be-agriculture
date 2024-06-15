1b4c23cd4a56900e9c462245d4a1b4bbefb2b834
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-06-15 20:20:01 +0700

    bug-fix

--- modules/watering_history/helper.go
+++ modules/watering_history/helper.go
@@ -12,6 +12,22 @@ func MapPlantToPlantResponse(plant *plant.Plant) *PlantResponse {
 		Sunlight:         plant.Sunlight,
 		PlantingTime:     plant.PlantingTime,
 		ClimateCondition: plant.ClimateCondition,
+		PlantImage:       MapPlantImagesToPlantImageResponses(plant.PlantImages),
 		CreatedAt:        plant.CreatedAt,
 	}
+}
+
+func MapPlantImagesToPlantImageResponses(images []plant.PlantImage) []PlantImageResponse {
+	var plantImageResponses []PlantImageResponse
+
+	for _, image := range images {
+		if image.IsPrimary == 1 {
+			plantImageResponses = append(plantImageResponses, PlantImageResponse{
+				ID:       image.ID,
+				FileName: image.FileName,
+			})
+		}
+	}
+
+	return plantImageResponses
 }
--- modules/watering_history/response.go
+++ modules/watering_history/response.go
@@ -14,13 +14,19 @@ type WateringHistoryResponse struct {
 }
 
 type PlantResponse struct {
-	ID               int       `json:"id" gorm:"primaryKey"`
-	Name             string    `json:"name"`
-	Description      string    `json:"description"`
-	IsToxic          bool      `json:"is_toxic"`
-	HarvestDuration  int       `json:"harvest_duration"`
-	Sunlight         string    `json:"sunlight"`
-	PlantingTime     string    `json:"planting_time"`
-	ClimateCondition string    `json:"climate_condition"`
-	CreatedAt        time.Time `json:"created_at"`
+	ID               int                  `json:"id" gorm:"primaryKey"`
+	Name             string               `json:"name"`
+	Description      string               `json:"description"`
+	IsToxic          bool                 `json:"is_toxic"`
+	HarvestDuration  int                  `json:"harvest_duration"`
+	Sunlight         string               `json:"sunlight"`
+	PlantingTime     string               `json:"planting_time"`
+	ClimateCondition string               `json:"climate_condition"`
+	PlantImage       []PlantImageResponse `json:"plant_image"`
+	CreatedAt        time.Time            `json:"created_at"`
+}
+
+type PlantImageResponse struct {
+	ID       int    `json:"id"`
+	FileName string `json:"file_name"`
 }
