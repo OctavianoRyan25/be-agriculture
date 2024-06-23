b20bb54c9d8b59ae5942e9c4297dc07838485bdc
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-06-23 13:53:14 +0700

    bug-fix

--- modules/article/cloudinary.go
+++ modules/article/cloudinary.go
@@ -2,8 +2,10 @@ package article
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
@@ -49,13 +51,29 @@ func DeleteFromCloudinary(filename string) error {
 		return err
 	}
 
+	publicID, _ := extractPublicID(filename)
+
 	// Delete image from Cloudinary
 	_, err = cld.Upload.Destroy(context.Background(), uploader.DestroyParams{
-		PublicID: filename,
+		PublicID: publicID,
 	})
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func extractPublicID(fileURL string) (string, error) {
+	parts := strings.Split(fileURL, "/")
+	for i, part := range parts {
+		if part == "upload" {
+			// Join all parts after "upload" except the file extension
+			publicID := strings.Join(parts[i+1:], "/")
+			// Remove the file extension
+			publicID = publicID[:strings.LastIndex(publicID, ".")]
+			return publicID, nil
+		}
+	}
+	return "", fmt.Errorf("invalid Cloudinary URL")
 }
